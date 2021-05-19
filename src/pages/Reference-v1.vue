d8dcdf6d29b2c83c77b95d4d65dd92a5ab44a2c2
EL0629 <borinaga.angelito0629@gmail.com>
2021-05-19 21:47:52 +0800

    update

--- src/pages/Reference-v1.vue
+++ src/pages/Reference-v1.vue
@@ -3,6 +3,20 @@
     <div v-bind:class="{'main': $q.platform.is.desktop, 'mobile_main': $q.platform.is.ipad || $q.platform.is.mobile}"
          class="full-height">
 
+      <q-header class="header q-py-sm" style="background-color: #FCE4EC;">
+        <img src="../../public/img/tinards-logo.png" width="120" height="50" class="q-p-lg q-ml-md q-mt-xs" />
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"
+          ><span class="navicon"></span
+        ></label>
+        <ul class="menu">
+          <li><a href="#work">Our Work</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#about-page">Careers</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </q-header>
+
       <q-header class="q-py-sm" style="background-color: #FCE4EC;">
         <q-toolbar class="mainmenuheader" height="60px">
           <img src="../../public/img/tinards-logo.png" width="120" height="50" class="q-p-xs q-ml-sm" />
