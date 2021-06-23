a9866d5a023abcb9d238b4c47495cbfa4cf7cc21
EL0629 <borinaga.angelito0629@gmail.com>
2021-06-23 18:58:07 +0800

    update

--- src/components/WebsiteForm/Tools/ProductSection.vue
+++ src/components/WebsiteForm/Tools/ProductSection.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout id="id_shop">
-    <template v-if="posts.length">
+    <template v-if="!loadingPosts && posts.length">
       <div class="text-h4 text-center text-weight-bolder q-py-sm q-my-md full-width"
             style="border-top:3px solid #ffffff;border-bottom: 3px solid #ffffff; color: #54595F;">Our Products
       </div>
@@ -25,8 +25,20 @@
       </div>
     </template>
 
-    <template v-else>
+    <template v-else-if="!loadingPosts && !posts.length">
       <h5 class="text-center text-grey">No posted products yet.</h5>
+    </template>
+
+    <template v-else>
+      <div class="flex flex-center">
+        <q-card flat bordered class="card-post q-mb-md q-mx-lg q-mb-xl">
+          <q-skeleton height="200px" square animation="fade" />
+          <q-card-section>
+            <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+            <q-skeleton type="text" width="50%" class="text-subtitle2" animation="fade" />
+          </q-card-section>
+        </q-card>
+      </div>
     </template>
     
   </q-layout>
@@ -36,18 +48,22 @@
 export default {
   data() {
     return {
-      posts: []
+      posts: [],
+      loadingPosts: false
     }
   },
   methods: {
     getPosts(){
+      this.loadingPosts = true
       this.$axios.get('https://tinardsbakeddelights-backend.herokuapp.com/posts').then(response => {
         this.posts = response.data
+        this.loadingPosts = false
       }).catch(err => {
         this.$q.dialog({
           title: 'Error',
           message: 'Could not find your products'
         })
+        this.loadingPosts = false
       })
     }
   },
